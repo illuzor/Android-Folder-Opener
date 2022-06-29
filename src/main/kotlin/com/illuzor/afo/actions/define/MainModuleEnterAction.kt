241d8842a27b705e4f49719038819113217ffc58
Artem Zanin <illuzor@gmail.com>
2022-06-29 20:30:14 +0300

    select and enter actions update

--- src/main/kotlin/com/illuzor/afo/actions/define/MainModuleEnterAction.kt
+++ src/main/kotlin/com/illuzor/afo/actions/define/MainModuleEnterAction.kt
@@ -1,6 +1,8 @@
 package com.illuzor.afo.actions.define
 
 import com.illuzor.afo.actions.BaseAction
+import com.illuzor.afo.ext.gradleFile
+import com.illuzor.afo.ext.isGradleModule
 import com.illuzor.afo.ext.notExists
 import com.illuzor.afo.ui.showErrorDialog
 import com.intellij.openapi.ui.Messages
@@ -9,24 +11,36 @@ import java.io.File
 internal class MainModuleEnterAction : BaseAction() {
 
     override fun perform() {
-        val moduleName = showInput()
+        val moduleName = showInput()?.replace(':', '/')
         if (moduleName.isNullOrEmpty()) {
             return
         }
 
-        val gradleConfigPath = project.basePath + "/" + moduleName + "/build.gradle"
-        val gradleConfigKtsPath = "$gradleConfigPath.kts"
-        if (File(gradleConfigPath).notExists() && File(gradleConfigKtsPath).notExists()) {
-            showErrorDialog("Module '$moduleName' Does Not Exists")
+        val modulePath = "$projectPath/$moduleName"
+        val moduleFolder = File(modulePath)
+        if (moduleFolder.notExists()) {
+            showErrorDialog("Directory '$modulePath' does not exist")
             return
         }
+
+        if (!moduleFolder.isGradleModule) {
+            showErrorDialog("Directory '$modulePath' is not gradle module")
+            return
+        }
+
+        val gradleFile = moduleFolder.gradleFile
+        if (gradleFile == null || gradleFile.notExists()) {
+            showErrorDialog("Directory '$modulePath' does not contain gradle file")
+            return
+        }
+
         projectPrefs.appModulePath = moduleName
     }
 
     private fun showInput(): String? =
         Messages.showInputDialog(
             project,
-            "Enter main module name",
+            "Enter main module name. For example 'module' or 'module:submodule'",
             "Main Module Name:",
             Messages.getQuestionIcon(),
         )
--- src/main/kotlin/com/illuzor/afo/actions/define/MainModuleSelectAction.kt
+++ src/main/kotlin/com/illuzor/afo/actions/define/MainModuleSelectAction.kt
@@ -1,27 +1,43 @@
 package com.illuzor.afo.actions.define
 
 import com.illuzor.afo.actions.BaseAction
+import com.illuzor.afo.ext.extractModules
+import com.illuzor.afo.ext.notExists
+import com.illuzor.afo.ext.settingsGradleFile
 import com.illuzor.afo.ui.ChooserDialogWrapper
 import com.illuzor.afo.ui.showErrorDialog
-import com.illuzor.afo.utils.ModulesUtil
+import java.io.File
 
 internal class MainModuleSelectAction : BaseAction() {
 
     override fun perform() {
-        val modules = ModulesUtil.getModulesList(projectPath)
+        val projectFolder = File(projectPath)
+        if (projectFolder.notExists() || projectFolder.listFiles().isNullOrEmpty()) {
+            showErrorDialog("Directory '$projectPath' is not gradle project")
+            return
+        }
+
+        val settingsGradleFile = projectFolder.settingsGradleFile
+        if (settingsGradleFile == null || settingsGradleFile.notExists()) {
+            showErrorDialog("Directory '$projectPath' does not contain settings.gradle or settings.gradle.kts file")
+            return
+        }
+
+        val settingsGradleString = settingsGradleFile.readText()
+        val modules = settingsGradleString.extractModules()
+
         if (modules.isEmpty()) {
             showErrorDialog("Unable to detect modules automatically")
         } else {
-            showChooser(modules)
+            showModuleChooser(modules)
         }
     }
 
-    private fun showChooser(modules: List<String>) {
+    private fun showModuleChooser(modules: List<String>) =
         ChooserDialogWrapper(project, modules).apply {
             onChoose { moduleName: String ->
                 projectPrefs.appModulePath = moduleName
             }
             show()
         }
-    }
 }
