d156c20a5e7b97bcf015f7cd6db17a9c24bbea55
Artem Zanin <illuzor@gmail.com>
2022-07-03 11:33:21 +0300

    ktlint added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/") // for ktlint-gradle plugin
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle")
+    }
+}
+
 plugins {
     id("java")
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     id("org.jetbrains.kotlin.jvm") version "1.7.0"
     id("org.jetbrains.intellij") version "1.6.0"
 }
@@ -9,6 +19,16 @@ version = "1.1.0"
 
 repositories {
     mavenCentral()
+}
+
+ktlint {
+    version.set("0.45.2")
+    disabledRules.set(
+        setOf(
+            "no-wildcard-imports",
+            "import-ordering",
+        )
+    )
 }
 
 intellij {
