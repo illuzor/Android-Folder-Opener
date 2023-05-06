6740fa9cce080c9c842e51ff170be32087217fcf
Artem Zanin <illuzor@gmail.com>
2023-05-06 21:45:39 +0600

    libs update

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ buildscript {
 
 plugins {
     id("java")
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
-    id("org.jetbrains.kotlin.jvm") version "1.7.21"
-    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
+    id("org.jetbrains.kotlin.jvm") version "1.8.21"
+    id("org.jetbrains.intellij") version "1.13.3"
 }
 
 group = "com.illuzor.afo"
@@ -32,7 +32,7 @@ ktlint {
 }
 
 intellij {
-    version.set("2021.2")
+    version.set("2021.3.3")
     type.set("IC")
     updateSinceUntilBuild.set(false)
 }
@@ -52,7 +52,7 @@ tasks {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.9.3"))
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
