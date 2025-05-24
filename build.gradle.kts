1d12d96ce6b3ec4037e01a7d909c657231241494
Artem Zanin <illuzor@gmail.com>
2025-05-24 21:00:27 +0500

    idea platform version 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,27 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("java")
     id("org.jlleitschuh.gradle.ktlint") version "12.3.0"
     id("org.jetbrains.kotlin.jvm") version "2.1.21"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.6.0"
 }
+
+val sinceBuildIdeaVersion = "213.7172"
+val verificationIdeaVersion = "2023.2.7"
 
 group = "com.illuzor.afo"
 version = "1.1.1"
 
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+        localPlatformArtifacts()
+    }
 }
 
 kotlin {
@@ -20,10 +32,21 @@ ktlint {
     version.set("1.6.0")
 }
 
-intellij {
-    version.set("2021.3.3")
-    type.set("IC")
-    updateSinceUntilBuild.set(false)
+intellijPlatform {
+    pluginConfiguration {
+        version = project.version.toString()
+        name = "Android Folder Opener"
+
+        ideaVersion {
+            sinceBuild = sinceBuildIdeaVersion
+        }
+    }
+
+    pluginVerification {
+        ides {
+            ide(IntelliJPlatformType.IntellijIdeaCommunity, version = verificationIdeaVersion)
+        }
+    }
 }
 
 tasks {
@@ -34,7 +57,7 @@ tasks {
 
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_11)
         }
     }
 
@@ -48,4 +71,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    intellijPlatform {
+        intellijIdeaCommunity(version = verificationIdeaVersion)
+        pluginVerifier()
+        testFramework(TestFrameworkType.Platform)
+    }
 }
